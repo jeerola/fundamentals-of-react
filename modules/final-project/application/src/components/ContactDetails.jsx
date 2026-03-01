43d5863d0baf2f7193055f60d577a3a19bb6dd6a
Jaakko Eerola <jaakko.t.eerola@tuni.fi>
2026-03-01 17:04:30 +0200

    Update ContactDetails.jsx

--- modules/final-project/application/src/components/ContactDetails.jsx
+++ modules/final-project/application/src/components/ContactDetails.jsx
@@ -1,4 +1,18 @@
-function ContactDetails() {
-  return <div>ContactDetails</div>;
+import { useParams } from "react-router-dom";
+
+function ContactDetails({ contacts }) {
+  const { id } = useParams();
+  const contact = contacts.find((c) => c.id.toString() === id);
+
+  if (!contact) return <p>Contact not found!</p>
+
+  return (
+    <div>
+      <h2>{contact.name}</h2>
+      <p>Phone: {contact.phone}</p>
+      <p>Email: {contact.email}</p>
+      <p>Address: {contact.address}</p>
+    </div>
+  );
 }
 export default ContactDetails;
